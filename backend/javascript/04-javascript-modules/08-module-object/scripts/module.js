2211e49bf87d8cae5b8b43739c55f703c4e971ed
RivanFadlani <rivanfadlani7@gmail.com>
2026-05-31 15:53:40 +0700

    study: 310526

---
+++ backend/javascript/04-javascript-modules/08-module-object/scripts/module.js
@@ -0,0 +1,12 @@
+export const firstName = "Rivan";
+export const lastName = "Fadlani";
+
+export function hello(name) {
+  console.info(`hello ${name}!`);
+}
+
+export class Person {
+  constructor(name) {
+    this.name = name;
+  }
+}
